70ec04a342dde3f35a4367305bf8244539d3db96
Akelvud <akulenko.slava@gmail.com>
2022-02-10 14:07:08 +0300

    fix: State.shootToString

--- modules/state/State.cpp
+++ modules/state/State.cpp
@@ -20,13 +20,15 @@ std::string State::shootToString(std::vector<std::shared_ptr<Tank>> tanks) {
 
     Hex hex = tanks[0]->getPosition();
     if (tank->getTankType() == TankType::AT_SPG) {
+        Hex hex_mis_dis = hex;
         int min_dis = 1e9;
         for (auto pos: tank->getPosition().neighbors) {
             if (hex.getDistance(*pos) < min_dis) {
                 min_dis = hex.getDistance(*pos);
-                hex = *pos;
+                hex_mis_dis = *pos;
             }
         }
+        hex = hex_mis_dis;
     }
 
     return "{\"type\":\"SHOOT\",\"data\":{\"vehicle_id\":" + std::to_string(tank->id) +
