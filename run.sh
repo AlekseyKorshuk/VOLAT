0e2d151e70170202e2f11b95585a77376ea358b2
AlekseyKorshuk <ale-kor02@mail.ru>
2022-03-13 23:57:28 +0300

    fix: strategy

--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@ cd ..
 if [[ "$OSTYPE" =~ ^msys ]]; then
     for file in $(find ./cmake-build-debug/*.exe) ; do
         if [ -x $file ] ; then
-            $file "VOLAT1" "" "testVOLAT" "99" "3" &
-            $file "VOLAT2" "" "testVOLAT" "99" "3" &
-            $file "VOLAT3" "" "testVOLAT" "99" "3" &
+            start $file "VOLAT1" "" "testVOLAT" "99" "3"
+            start $file "VOLAT2" "" "testVOLAT" "99" "3"
+            start $file "VOLAT3" "" "testVOLAT" "99" "3"
         fi
     done
 else
