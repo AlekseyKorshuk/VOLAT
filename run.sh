67e7bca6627c22b4307bc00b47cd3c822012896a
AlekseyKorshuk <ale-kor02@mail.ru>
2022-03-13 23:47:50 +0300

    fix: strategy

--- run.sh
+++ run.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 cd visualizer
-python visualizer.py  --game_name "testVOLAT2" --password "" --num_turns 99 --num_players 3 --hide_gui &
+python visualizer.py  --game_name "testVOLAT" --password "" --num_turns 99 --num_players 3 --hide_gui &
 cd ..
 
 if [[ "$OSTYPE" =~ ^msys ]]; then
     for file in $(find ./cmake-build-debug/*.exe) ; do
         if [ -x $file ] ; then
-            $file "VOLAT1" "" "testVOLAT2" "99" "3" "0"
+            $file "VOLAT1" "" "testVOLAT" "99" "3" &
+            $file "VOLAT2" "" "testVOLAT" "99" "3" &
+            $file "VOLAT3" "" "testVOLAT" "99" "3" &
         fi
     done
 else
       for file in $(find ./cmake-build-debug/ -type f) ; do
           if [ -x $file ] ; then
-              $file "VOLAT1" "" "testVOLAT2" "99" "3" "0"
+              $file "VOLAT1" "" "testVOLAT" "99" "3" &
+              $file "VOLAT2" "" "testVOLAT" "99" "3" &
+              $file "VOLAT3" "" "testVOLAT" "99" "3" &
           fi
       done
 fi
